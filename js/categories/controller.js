c75571a13b55b2878788175ea37143899828d510
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-02 18:31:14 -0700

    Add sample categories

--- js/categories/controller.js
+++ js/categories/controller.js
@@ -1,1 +1,17 @@
 angular.module('categories', ['ngMaterial'])
+  .controller('CategoriesCtrl', function($scope) {
+    $scope.categories = [
+      {
+        label: 'All'
+      },
+      {
+        label: 'Restaurants'
+      },
+      {
+        label: 'Bars'
+      },
+      {
+        label: 'Coffee shops'
+      },
+    ];
+  });
