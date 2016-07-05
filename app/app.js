431c27d180863fab99c455be9d2cf75f259b9f2c
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-04 21:50:49 -0700

    List out dependencies in newlines

--- app/app.js
+++ app/app.js
@@ -1,5 +1,12 @@
 angular
-  .module('savedPlaces', ['ngMaterial', 'ngRoute', 'ngResource', 'categories', 'content', 'explore'])
+  .module('savedPlaces', [
+    'ngMaterial',
+    'ngRoute',
+    'ngResource',
+    'categories',
+    'content',
+    'explore',
+  ])
   .run(function($log){
     $log.debug("running");
   });
