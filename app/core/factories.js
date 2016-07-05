54953889d4001e444192b941dbc59fced15b2ca6
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-04 20:24:22 -0700

    Ask for Google API Key if needed until not blank

--- app/core/factories.js
+++ app/core/factories.js
@@ -25,12 +25,18 @@ angular.module('savedPlaces')
   .factory('googlePlaces', ['$window', '$resource', 'apiKeys', function ($window, $resource, apiKeys) {
     // API key as closure variable so asks only once
     // Prompt to enter API key if user has forgotten to create new app/api/keys.js file
+      // Keep asking if not provided
     var apiKey = (apiKeys.googlePlaces === 'API KEY HERE') ?
-        $window.prompt('Input your Google API key') :
+        askForAPIKey($window) :
         apiKeys.googlePlaces;
 
     return {
       getNearbyPlaces: getNearbyPlaces,
+    }
+
+    function askForAPIKey($window) {
+      var ans = $window.prompt('Input your Google API key');
+      return ans ? ans : askForAPIKey($window);
     }
 
     function getNearbyPlaces($resource) {
