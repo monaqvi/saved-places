9faaf570891f001f56e2e66998392a1d46fcc4eb
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-04 17:50:50 -0700

    Add skeleton function for Google Places API call

--- app/core/factories.js
+++ app/core/factories.js
@@ -1,25 +1,32 @@
 angular.module('savedPlaces')
   .factory('geoLocator', ['$q', '$window', function ($q, $window) {
     return {
-        getCurrentPosition: getCurrentPosition
+      getCurrentPosition: function () {
+        var deferred = $q.defer();
+
+        if (!$window.navigator.geolocation) {
+          deferred.reject('Geolocation not supported.');
+        } else {
+          $window.navigator.geolocation.getCurrentPosition(
+            function (position) {
+              // console.log(position);
+              deferred.resolve(position);
+            },
+            function (err) {
+              deferred.reject(err);
+            }
+          );
+        }
+        
+        return deferred.promise;
+      }
+    }
+  }])
+  .factory('googlePlaces', ['$resource', function ($resource) {
+    return {
+        getNearbyPlaces: getNearbyPlaces
     };
 
-    function getCurrentPosition() {
-      var deferred = $q.defer();
-
-      if (!$window.navigator.geolocation) {
-        deferred.reject('Geolocation not supported.');
-      } else {
-        $window.navigator.geolocation.getCurrentPosition(
-          function (position) {
-            // console.log(position);
-            deferred.resolve(position);
-          },
-          function (err) {
-            deferred.reject(err);
-          }
-        );
-      }
-      return deferred.promise;
+    function getNearbyPlaces() {
     }
-}]);
+  }]);
