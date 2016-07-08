6cbf141c4bba70df351d96833c7594c207f8551e
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-08 14:45:02 -0700

    Add newPlaceDialogueBox without functionality

--- app/newPlace/controller.js
+++ app/newPlace/controller.js
@@ -1,4 +1,12 @@
 angular.module('newPlace')
-  .controller('NewPlaceCtrl', ['$scope', function($scope) {
-    
-  }]);
+  .controller('NewPlaceCtrl', ['$scope',
+    function ($scope) {
+      $scope.address = '';
+      $scope.details = '';
+      $scope.options = {
+        country: 'us',
+        // bounds: ''
+      };
+    }
+  ]);
+
