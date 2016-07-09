bf858bb7a10dde520b4099e36e6d8b92f296f5d5
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-09 15:22:22 -0700

    Add dummy center

--- app/newPlace/controller.js
+++ app/newPlace/controller.js
@@ -3,6 +3,7 @@ angular.module('newPlace')
     function ($scope, categoryList) {
       // Take out 'All' (first item) as a valid option
       $scope.categories = categoryList.slice(1);
+      $scope.center = '41.850033,-87.6500523';
 
       $scope.address = '';
       $scope.details = '';
