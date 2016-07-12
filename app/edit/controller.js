4c054ab898f9f5309503ccbdacb14249ada2471f
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-12 15:28:14 -0700

    Add editing functionality to savedPlaces

--- app/edit/controller.js
+++ app/edit/controller.js
@@ -1,6 +1,26 @@
 angular.module('edit')
   .controller('EditPlaceCtrl', ['$scope', '$routeParams', '$mdDialog', 'categoryList', 'savedPlaces',
     function ($scope, $routeParams, $mdDialog, categoryList, savedPlaces) {
+      var placeIndex = $routeParams.placeId;
+      var activePlace = savedPlaces[placeIndex];
+
+
+      $scope.categories = categoryList.slice(1);
+      $scope.address = activePlace.address;
+      $scope.placeName = activePlace.name;
+      $scope.placeCategory = activePlace.category;
+      $scope.placeNote = activePlace.note;
+
+      $scope.savePlace = function() {
+        var newInfo = {
+          name: $scope.placeName,
+          address: $scope.address,
+          category: $scope.placeCategory,
+          note: $scope.placeNote,
+        };
+
+        savedPlaces[placeIndex] = Object.assign(activePlace, newInfo);
+      }
     }
   ]);
   
