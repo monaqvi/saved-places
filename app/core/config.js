91fd2124a130c12b909a91b1b1153979c13e1255
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-12 14:48:53 -0700

    Rename newPlaceTest -> newPlacePrompt

--- app/core/config.js
+++ app/core/config.js
@@ -10,7 +10,7 @@ angular.module('savedPlaces')
         activeView: 'places'
       })
       .when('/newPlace', {
-        templateUrl: './app/newPlace/newPlaceTest.html',
+        templateUrl: './app/newPlace/newPlacePrompt.html',
         controller: 'NewPlaceCtrl',
         activeView: 'places'
       })
