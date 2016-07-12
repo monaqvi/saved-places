7ca8cc1504eace493a5a18668d2856ad386931a1
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-07-12 15:25:08 -0700

    Add address property to place

--- app/content/factories.js
+++ app/content/factories.js
@@ -16,6 +16,7 @@ angular.module('content')
       
       newPlace = {
         name: opts.name,
+        address: opts.address,
         note: opts.note,
         photo: opts.photo,
         category: opts.category,
